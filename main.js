33b9009f1dcdb02528c1fcadb835400f794f51f4
Javi-Valdivia <101889568+Javi-Valdivia@users.noreply.github.com>
2023-03-08 23:56:24 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function enfocarSeccion1() {
+    var seccion = document.getElementById('about');
+    seccion.scrollIntoView({ behavior: "smooth" });
+}
+function enfocarSeccion3() {
+    var seccion = document.getElementById('skill');
+    seccion.scrollIntoView({ behavior: "smooth" });
+}
+function enfocarSeccion2() {
+    var seccion = document.getElementById('experience');
+    seccion.scrollIntoView({ behavior: "smooth" });
+}
+function enfocarSeccion4() {
+    var seccion = document.getElementById('contact');
+    seccion.scrollIntoView({ behavior: "smooth" });
+}
